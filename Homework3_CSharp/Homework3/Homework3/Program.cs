2ac27f943bb740838f22ef99d1d92a10ee4eed89
Lotboks <Aleks.dostinov@gmail.com>
2023-03-13 16:17:18 +0100

    Homework 3-4

---
+++ Homework3_CSharp/Homework3/Homework3/Program.cs
@@ -0,0 +1,76 @@
+﻿#region
+
+//int[] numbersArray = new int[6];
+//Console.WriteLine("Enter 6 numbers:");
+
+//for (int i = 0; i < numbersArray.Length; i++)
+//{
+//    while (true)
+//    {
+//        if (int.TryParse(Console.ReadLine(), out int input)) // Eve i so TryParse da ne bide samo so convert.to :)
+//        {
+//            numbersArray[i] = input;
+//            break;
+//        }
+//        else
+//        {
+//            Console.WriteLine("Invalid input. Please enter a number.");
+//        }
+//    }
+//}
+//int evenSum = 0;
+
+//for (int i = 0; i < numbersArray.Length; i++)
+//{
+//    if (numbersArray[i] % 2 == 0)
+//    {
+//        evenSum += numbersArray[i];
+//    }
+//}
+
+//Console.WriteLine($"The sum of the even numbers is: {evenSum}");
+
+#endregion
+
+#region
+
+string[] studentsG1 = { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = { "Mile", "Cacko", "Toso", "Dzoni", "Blazo" };
+
+Console.Write("Enter student group: (there are 1 and 2): ");
+
+int groupNumber;
+if (int.TryParse(Console.ReadLine(), out groupNumber))
+{
+    if (groupNumber == 1)
+    {
+        Console.WriteLine("The Students in G1 are:");
+
+        foreach (string students in studentsG1)
+        {
+            Console.WriteLine(students);
+        }
+    }
+
+    else if (groupNumber == 2)
+    {
+        Console.WriteLine("The Students in G2 are:");
+
+        foreach (string students in studentsG2)
+        {
+            Console.WriteLine(students);
+        }
+    }
+
+    else
+    {
+        Console.WriteLine("Invalid group number. Please enter 1 or 2.");
+    }
+}
+
+else
+{
+    Console.WriteLine("Invalid input. Please enter a number between 1 and 2.");
+}
+
+#endregion
---
+++ Homework4_CSharp/Homework4/Homework4/Program.cs
@@ -0,0 +1,54 @@
+﻿
+#region
+
+//Console.WriteLine("Enter a string longer then 5 characters: ");
+//string stringInput = Convert.ToString(Console.ReadLine());
+
+//if (stringInput.Length >= 5)
+//{
+//    string lastFiveChar = stringInput.Substring(stringInput.Length - 5);
+//    Console.WriteLine("Last 5 characters: " + lastFiveChar);
+//} else
+//{
+//    Console.WriteLine("Input string is too short");
+//}
+
+#endregion
+
+#region
+
+//Console.Write("Enter a sentence: ");
+//string sentenceInput = Console.ReadLine();
+
+//string[] words = sentenceInput.Split(' ');
+
+//Console.WriteLine("Words in the sentence: ");
+//foreach (string word in words)
+//{
+//    Console.WriteLine(word);
+//}
+
+#endregion
+
+#region
+
+static int SumOfDigits()
+{
+    Console.Write("Enter a number: ");
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    int sumOfDigits = 0;
+
+    string numberString = inputNumber.ToString();
+
+    foreach (char digit in numberString)
+    {
+        sumOfDigits += Convert.ToInt32(digit.ToString());
+    }
+
+    return sumOfDigits;
+}
+
+int sum = SumOfDigits();
+Console.WriteLine("Sum of digits: {0}", sum);
+
+#endregion
